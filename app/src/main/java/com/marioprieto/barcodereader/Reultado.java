0807fac65bbd24b5a53b8be1c04238806faba58d
Mario <prietosanlesmario99@gmail.com>
2019-11-21 23:47:18 +0100

    Resoult activity added -- No funtional

---
+++ app/src/main/java/com/marioprieto/barcodereader/Reultado.java
@@ -0,0 +1,14 @@
+package com.marioprieto.barcodereader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reultado);
+    }
+}
